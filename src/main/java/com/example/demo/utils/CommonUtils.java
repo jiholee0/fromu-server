1a103bfafe6a0706122a941bfe7d05608e6704aa
jiholee0 <dlwlgh1254@naver.com>
2023-02-22 19:51:52 +0900

    [USER] BUILD ERROR 해결

--- src/main/java/com/example/demo/utils/CommonUtils.java
+++ src/main/java/com/example/demo/utils/CommonUtils.java
@@ -4,8 +4,9 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import java.time.LocalTime;
 
+
 public class CommonUtils {
-    public String createUserCode(){
+    public static String createUserCode(){
         String userCode = RandomStringUtils.randomAlphabetic(3) + String.format("%02d", LocalTime.now().getSecond()) + RandomStringUtils.randomAlphabetic(3);
         return userCode;
     }
