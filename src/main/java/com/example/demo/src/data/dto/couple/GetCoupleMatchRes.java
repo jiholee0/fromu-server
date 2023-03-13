1c35777b43bac9c8480b53141b2016953f4f6287
jiholee0 <dlwlgh1254@naver.com>
2023-03-14 07:19:18 +0900

    [COUPLE] isMatch api response 에 dDay 추가

--- src/main/java/com/example/demo/src/data/dto/couple/GetCoupleMatchRes.java
+++ src/main/java/com/example/demo/src/data/dto/couple/GetCoupleMatchRes.java
@@ -10,9 +10,12 @@ public class GetCoupleMatchRes {
     private boolean isMatch;
     @ApiModelProperty(allowableValues = "null", notes = "nullable")
     private CoupleRes coupleRes;
+    @ApiModelProperty(allowableValues = "dDay 설정이 아직 되지 않았으면 0")
+    private int dDay;
 
-    public GetCoupleMatchRes(boolean isMatch, CoupleRes coupleRes){
+    public GetCoupleMatchRes(boolean isMatch, CoupleRes coupleRes, int dDay){
         this.isMatch = isMatch;
         this.coupleRes = coupleRes;
+        this.dDay = dDay;
     }
 }
