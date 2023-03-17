dd6aa7a1cc21e1b4c00528e0db62e12553377dcf
jiholee0 <dlwlgh1254@naver.com>
2023-03-17 20:06:30 +0900

    에러 수정

--- src/main/java/com/example/demo/src/data/entity/UserRepository.java
+++ src/main/java/com/example/demo/src/data/entity/UserRepository.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
     @NotNull List<User> findAll();
     Optional<User> findById(int userId);
+    @Where(clause = "delete_flag = false")
     Optional<User> findByEmail(String email);
+    @Where(clause = "delete_flag = false")
     Optional<User> findByUserCode(String userCode);
 }
